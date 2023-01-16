9416af61ec5f1f15a3956805e2d27d592acfa71f
Naser Behnam <122173123+Behnamxlto1@users.noreply.github.com>
2023-01-17 02:47:23 +0330

    Add files via upload

---
+++ python 4.py
@@ -0,0 +1,18 @@
+import random
+
+pc_number = random.randint(1,20)
+n = 1
+
+while True:
+    user_number = int(input("Guess a number between 1 and 20 chances:"))
+    
+    n += 1
+
+    if pc_number == user_number:
+        print("Well done, you guessed right. The number of your guesses :", n)
+        break
+
+    if user_number < pc_number :
+        print ("The desired number is higher")
+    else:
+        print ("The desired number is less")
